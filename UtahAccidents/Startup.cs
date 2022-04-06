2e3f33e7a99956e136b00c89c12ac539d8c3bf76
mckay-moulton <79293626+mckay-moulton@users.noreply.github.com>
2022-04-06 08:46:17 -0600

    cookies

--- UtahAccidents/Startup.cs
+++ UtahAccidents/Startup.cs
@@ -26,6 +26,13 @@ namespace UtahAccidents
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
             services.AddControllersWithViews();
             services.AddDbContext<AccidentsDbContext>(options =>
            {
